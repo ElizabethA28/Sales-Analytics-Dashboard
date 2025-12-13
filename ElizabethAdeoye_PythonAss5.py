66da5e17a807cbba2ee2f615c833a1db37b8e8e1
ElizabethA28 <eadeoye28@gmail.com>
2025-12-12 20:33:49 -0500

    Add files via upload

--- ElizabethAdeoye_PythonAss5.py
+++ ElizabethAdeoye_PythonAss5.py
@@ -13,7 +13,6 @@ if uploaded_file is not None:
     # Read the file depending on the type
     if uploaded_file.name.endswith(".csv"):
         df = pd.read_csv(uploaded_file, encoding="utf-8", errors="replace")
-        # If utf-8 fails, try: df = pd.read_csv(uploaded_file, encoding="latin1")
     else:
         df = pd.read_excel(uploaded_file, engine="openpyxl")
 
